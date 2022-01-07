a105c1a9207a28d9cf5baa294202bde9e8696ed9
Bulat <davlyatshinbulat@gmail.com>
2022-01-08 02:25:18 +0500

    initial

---
+++ src/main/java/uz/episodeone/merchants/MerchantsApplication.java
@@ -0,0 +1,11 @@
+package uz.episodeone.merchants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantsApplication.class, args);
+    }
+}
