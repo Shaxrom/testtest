0a622a8080df5af75d757af380a6cc80a9985cd9
Shaxrom Shukurov <shaxromshukurov@gmail.com>
2022-01-10 22:59:07 +0500

    ref last migr

--- src/main/resources/db/migration/V20220110171008__add_merchant.sql
+++ src/main/resources/db/migration/V20220110171008__add_merchant.sql
@@ -1,10 +1,2 @@
-insert into public.service(
-id,uzcard_merchant_id,
-uzcard_terminal_id,
-humo_merchant_id,
-humo_terminal_id)
-values (1,
-'uzcard_merchant_test',
-'uzcard_terminal_test',
-'humo_merchant_test',
-'humo_terminal_test');
+insert into public.service(id,uzcard_merchant_id, uzcard_terminal_id, humo_merchant_id, humo_terminal_id)
+values (1, '90040025565', '93000151', '010950461213101', '09610001');
