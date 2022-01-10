a05f5a4286184468955ae52ec9c900837748018b
Bulat <davlyatshinbulat@gmail.com>
2022-01-11 03:10:17 +0500

    fields

--- src/main/java/uz/episodeone/merchants/dto/InitBillingDto.java
+++ src/main/java/uz/episodeone/merchants/dto/InitBillingDto.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class InitBillingDto {
     private Long serviceId;
-    private String userAccount;
+    private Map<String, String> fields;
 }
--- src/main/java/uz/episodeone/merchants/dto/SubmitPaymentDto.java
+++ src/main/java/uz/episodeone/merchants/dto/SubmitPaymentDto.java
@@ -2,7 +2,11 @@ package uz.episodeone.merchants.dto;
 
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class SubmitPaymentDto {
     private String billingTransactionId;
+    private Long serviceId;
+    private Map<String, String> fields;
 }
