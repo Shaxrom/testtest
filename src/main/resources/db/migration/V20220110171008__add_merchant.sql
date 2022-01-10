a8fdcea4196b623b5bc611fa5f34c9d6a15b23cb
Shaxrom Shukurov <shaxromshukurov@gmail.com>
2022-01-10 17:17:17 +0500

    add merchant

---
+++ src/main/resources/db/migration/V20220110171008__add_merchant.sql
@@ -0,0 +1,10 @@
+insert into public.service(
+id,uzcard_merchant_id,
+uzcard_terminal_id,
+humo_merchant_id,
+humo_terminal_id)
+values (1,
+'uzcard_merchant_test',
+'uzcard_terminal_test',
+'humo_merchant_test',
+'humo_terminal_test');
